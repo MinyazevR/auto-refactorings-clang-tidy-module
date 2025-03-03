cc1e76b22f1f4b9650cb06c44da79e06a87ded01
MinyazevR <eeuriset8@gmail.com>
2025-03-03 19:17:30 +0300

    aaa

--- run-clang-format.sh
+++ run-clang-format.sh
@@ -17,4 +17,4 @@ done
 
 IGNORE_CMD=${IGNORE_CMD% -o}
 
-find . \( $IGNORE_CMD \) -prune -o -name '*.cpp' -o -name '*.h' -print0 | xargs -0 clang-format${1+-$1} --style=file -i
+find . \( $IGNORE_CMD \) -prune -o -name '*.cpp' -print0 -o -name '*.h' -print0 | xargs -0 clang-format${1+-$1} --style=file -i
