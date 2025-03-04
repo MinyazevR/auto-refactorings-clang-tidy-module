7795bc31845389154cce51dd8d267b3db258daf4
MinyazevR <eeuriset8@gmail.com>
2025-03-04 04:38:58 +0300

    Fix compiler error

--- src/IfElseReturnChecker.h
+++ src/IfElseReturnChecker.h
@@ -2,14 +2,8 @@
 #define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_AUTOREFACTORING_IFELSECHECK_H
 
 #include "../ClangTidyCheck.h"
-#include "clang/Rewrite/Core/Rewriter.h"
 
-class ASTContext;
-class CFGStmtMap;
-class IfStmt;
-class CFG;
-
-using namespace clang;
+class Rewriter;
 
 namespace clang::tidy::autorefactorings {
 
@@ -22,30 +16,33 @@ public:
   void storeOptions(ClangTidyOptions::OptionMap &Opts) override;
   void registerMatchers(ast_matchers::MatchFinder *Finder) override;
   void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
-  void runInternal(IfStmt *ifStmt,
+  void runInternal(clang::IfStmt *ifStmt,
                    const ast_matchers::MatchFinder::MatchResult &Result,
-                   CFG &cfg);
+                   clang::CFG &cfg);
 
 private:
   ulong mIndent = 2;
   bool mNeedShift{};
-  Rewriter Rewrite;
+  std::unique_ptr<Rewriter> mRewrite;
   std::list<clang::FixItHint> mFixList;
-  bool reversCondition(const IfStmt *ifStmt, const SourceManager *manager);
+  bool reversCondition(const clang::IfStmt *ifStmt,
+                       const SourceManager *manager);
 
-  void appendStmt(const CompoundStmt *stmt, const Stmt *stmtToAdd,
+  void appendStmt(const clang::CompoundStmt *stmt, const clang::Stmt *stmtToAdd,
                   const clang::SourceManager *manager,
                   const clang::ASTContext *context);
 
   void indentBlock(const CompoundStmt *stmt, const std::string &string,
                    unsigned long indent);
 
-  void reverseStmt(const IfStmt *ifStmt, const clang::ASTContext &context,
+  void reverseStmt(const clang::IfStmt *ifStmt,
+                   const clang::ASTContext &context,
                    const clang::SourceManager *manager, bool needShift);
 
-  bool addIterruptionBlockToStmts(const Stmt *stmt, const CFG &cfg,
+  bool addIterruptionBlockToStmts(const clang::Stmt *stmt,
+                                  const clang::CFG &cfg,
                                   const clang::SourceManager *manager,
-                                  const CFGStmtMap *stmtToBlockMap,
+                                  const clang::CFGStmtMap *stmtToBlockMap,
                                   const clang::ASTContext *context);
 };
 }; // namespace clang::tidy::autorefactorings
