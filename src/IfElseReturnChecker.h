02e3d6f86258c16692ad9d70d32d15ed98412196
MinyazevR <89993880+MinyazevR@users.noreply.github.com>
2025-03-05 16:10:22 +0300

    Fix all clang-tidy warnings (#4)

--- src/IfElseReturnChecker.h
+++ src/IfElseReturnChecker.h
@@ -1,5 +1,5 @@
-#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_AUTOREFACTORING_IFELSECHECK_H
-#define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_AUTOREFACTORING_IFELSECHECK_H
+#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_AUTOREFACTORINGS_IFELSERETURNCHECKER_H
+#define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_AUTOREFACTORINGS_IFELSERETURNCHECKER_H
 
 #include "../ClangTidyCheck.h"
 #include "clang/Rewrite/Core/Rewriter.h"
@@ -20,35 +20,69 @@ public:
   void storeOptions(ClangTidyOptions::OptionMap &Opts) override;
   void registerMatchers(ast_matchers::MatchFinder *Finder) override;
   void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
-  void runInternal(IfStmt *ifStmt,
+  void runInternal(IfStmt *IfStmt,
                    const ast_matchers::MatchFinder::MatchResult &Result,
-                   clang::CFG &cfg);
+                   clang::CFG &Cfg);
 
 private:
-  ulong mIndent = 2;
-  bool mNeedShift{};
-  std::unique_ptr<Rewriter> mRewrite;
-  std::list<clang::FixItHint> mFixList;
-  bool reversCondition(const clang::IfStmt *ifStmt,
-                       const SourceManager *manager);
+  // The offset required when removing the else block is set by the user in
+  // .clang-tidy.
+  uint32_t Indent = 2;
 
-  void appendStmt(const clang::CompoundStmt *stmt, const clang::Stmt *stmtToAdd,
-                  const clang::SourceManager *manager,
-                  const clang::ASTContext *context);
+  // Whether it is necessary to move the else block is set by the user in
+  // .clang-tidy.
+  bool NeedShift{};
 
-  void indentBlock(const CompoundStmt *stmt, const std::string &string,
-                   unsigned long indent);
+  // A rewriter used to track intermediate changes to implement changes to all
+  // IfStmts in one pass when reversining surrounding IfStmts, you need to have
+  // information about internal ones
+  std::unique_ptr<Rewriter> Rewrite;
+  std::list<clang::FixItHint> FixList;
 
-  void reverseStmt(const clang::IfStmt *ifStmt,
-                   const clang::ASTContext &context,
-                   const clang::SourceManager *manager, bool needShift);
+  /// The method required to reverse the ifStmt condition
+  /// In the simplest case, it just adds !( beginning of if condition and ) at
+  /// the end. The C language is supposed to support a more beautiful style by
+  /// type: if (x > y) -> if (x <= y) What is difficult in C++ due to operator
+  /// overloading.
+  bool reversCondition(const IfStmt *IfStmt, const SourceManager *Manager);
 
-  bool addIterruptionBlockToStmts(const clang::Stmt *stmt,
-                                  const clang::CFG &cfg,
-                                  const clang::SourceManager *manager,
-                                  const clang::CFGStmtMap *stmtToBlockMap,
-                                  const clang::ASTContext *context);
+  /// Accepts a compound statement and replaces it in the source code with a
+  /// string, shifting to the left by indent param.
+  void moveBlock(const CompoundStmt *Stmt, const std::string &String,
+                 unsigned long Indent);
+
+  /// Add stmt to the original stmt, which is used to pull up blocks.
+  ///
+  /// For example:
+  /// if () {                            if () {
+  ///    int x = 1;                          int x = 1;
+  /// }               ---------------->      return 1;
+  /// else {                             }
+  ///    int y = 1;                      else {
+  /// }                                      int y = 1;
+  /// return 1;                          }
+  ///                                    return 1;
+  void appendStmt(const CompoundStmt *CmpStmt, const Stmt *StmtToAdd,
+                  const SourceManager *Manager, const ASTContext *Context);
+
+  /// A method for reverse IfStmt
+  ///
+  /// For example:
+  /// if () {                            if () {
+  ///    int x = 1;                          int y = 1;
+  /// }               ---------------->  }
+  /// else {                             else {
+  ///    int y = 1;                          int x = 1;
+  /// }                                  }
+  /// return 1;                          return 1;
+  void reverseStmt(const IfStmt *IfStmt, const ASTContext &Context,
+                   const SourceManager *Manager);
+
+  bool addBlockToStmt(const Stmt *Stmt, const clang::CFG &Cfg,
+                      const SourceManager *Manager,
+                      const clang::CFGStmtMap *StmtToBlockMap,
+                      const ASTContext *Context);
 };
 }; // namespace clang::tidy::autorefactorings
 
-#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_AUTOREFACTORING_IFELSECHECK_H
+#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_AUTOREFACTORINGS_IFELSERETURNCHECKER_H
