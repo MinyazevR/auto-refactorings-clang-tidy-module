01145a6effb84f39d3e46cc344423816fcdefb4d
MinyazevR <eeuriset8@gmail.com>
2025-03-18 23:35:21 +0300

    Fix tests

--- tests/rename-ghidra-variable-checker/basic-tests/input_struct_member_access.c
+++ tests/rename-ghidra-variable-checker/basic-tests/input_struct_member_access.c
@@ -2,7 +2,19 @@
 #include "basic_test.h"
 
 int main(int argc, char** argv) {
-  test_struct_t t;
+  pack_test_struct_t t;
   t._0_1 = 'c';
   t._1_4 = 123;
+  
+  pack_test_struct_t* y;
+  y->_0_1 = 'c';
+  y->_1_4 = 123;
+  
+  test_struct_t z;
+  z._0_1 = 'c';
+  z._4_4 = 123;
+  
+  test_struct_t* u;
+  u->_0_1 = 'c';
+  u->_4_4 = 123;
 }
